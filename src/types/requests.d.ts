a0da8d022e819982cb95fd43ca42407f0593fa65
unknown <marksmersh01@gmail.com>
2022-12-03 22:38:31 +0100

    Refactor types

--- src/types/requests.d.ts
+++ src/types/requests.d.ts
@@ -229,7 +229,7 @@ export interface RequestTypes {
         request: {
             chat_id: string | number, message_thread_id?: number, from_chat_id: string | number,
             message_id: number, caption?: string, parse_mode?: string, caption_entities?: MessageEntity[],
-            disable_notification?: boolean, protect_content: boolean, reply_to_message_id: number,
+            disable_notification?: boolean, protect_content?: boolean, reply_to_message_id?: number,
             allow_sending_without_reply?: boolean, reply_markup?: string 
         },
         response: MessageId
