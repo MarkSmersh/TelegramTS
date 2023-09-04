1fe6a4a2531ad5dfd222ed4ce9f637ed52d9e463
Say my name <marksmersh01@gmail.com>
2023-09-04 17:10:52 +0200

    add getChatAdministrators and getChatMember methods

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,81 +1,133 @@
-import { IMessage, IChat, IUser, IBasicResponse, IInlineKeyboardMarkup, IFile, IUpdate } from ".";
+import {
+    IMessage,
+    IChat,
+    IUser,
+    IBasicResponse,
+    IInlineKeyboardMarkup,
+    IFile,
+    IUpdate,
+    IChatMember,
+} from ".";
 
 export interface ResponseEvents {
-    load: [],
-    start:  [ message: IUser ],
-    update: [ update: IUpdate ],
-    error: [ error: IBasicResponse ]
+    load: [];
+    start: [message: IUser];
+    update: [update: IUpdate];
+    error: [error: IBasicResponse];
 }
 
-type ParseModes = "MarkdownV2"| "HTML" | "Markdown";
+type ParseModes = "MarkdownV2" | "HTML" | "Markdown";
 
 export interface RequestTypes {
     getMe: {
-        request: {}, response: IUser 
-    },
+        request: {};
+        response: IUser;
+    };
     getUpdates: {
-        request: { offset?: number, limit?: number, timeout?: number, allowed_updates?: string[] }, 
-        response: IUpdate[] 
-    },
-    sendMessage: { 
-        request: 
-            { 
-                chat_id: number | string | undefined, text: string | undefined, parse_mode?: ParseModes, 
-                entities?: MessageEventInit[], disable_web_page_preview?: boolean, disable_notification?: boolean,
-                protect_content?: boolean, reply_to_message_id?: number, allow_sending_without_reply?: boolean,
-                reply_markup?: IInlineKeyboardMarkup | string
-            },
-        response: IMessage 
-    },
-    editMessageText: { 
-        request: 
-        {
-            chat_id: number | string | undefined, message_id: number, text: string, inline_message_id?: string, 
-            parse_mode?: ParseModes, entites?: MessageEventInit[], disable_web_page_preview?: boolean,
-            reply_markup?: string 
-        },
-        response: IMessage 
-    },
+        request: {
+            offset?: number;
+            limit?: number;
+            timeout?: number;
+            allowed_updates?: string[];
+        };
+        response: IUpdate[];
+    };
+    sendMessage: {
+        request: {
+            chat_id: number | string | undefined;
+            text: string | undefined;
+            parse_mode?: ParseModes;
+            entities?: MessageEventInit[];
+            disable_web_page_preview?: boolean;
+            disable_notification?: boolean;
+            protect_content?: boolean;
+            reply_to_message_id?: number;
+            allow_sending_without_reply?: boolean;
+            reply_markup?: IInlineKeyboardMarkup | string;
+        };
+        response: IMessage;
+    };
+    editMessageText: {
+        request: {
+            chat_id: number | string | undefined;
+            message_id: number;
+            text: string;
+            inline_message_id?: string;
+            parse_mode?: ParseModes;
+            entites?: MessageEventInit[];
+            disable_web_page_preview?: boolean;
+            reply_markup?: string;
+        };
+        response: IMessage;
+    };
     answerCallbackQuery: {
-        request: 
-        {
-            callback_query_id: string, text?: string, show_alert?: boolean, url?: string,
-            cache_time?: number
-        },
-        response: true
-    },
+        request: {
+            callback_query_id: string;
+            text?: string;
+            show_alert?: boolean;
+            url?: string;
+            cache_time?: number;
+        };
+        response: true;
+    };
     deleteMessage: {
         request: {
-            chat_id: number | string | undefined, message_id: string
-        },
-        response: true
-    },
+            chat_id: number | string | undefined;
+            message_id: string;
+        };
+        response: true;
+    };
     forwardMessage: {
         request: {
-            chat_id: number | string | undefined, message_thread_id?: number, from_chat_id: string | number,
-            disable_notification?: boolean, protect_content?: boolean, message_id: number
-        },
-        response: IMessage
-    },
+            chat_id: number | string | undefined;
+            message_thread_id?: number;
+            from_chat_id: string | number;
+            disable_notification?: boolean;
+            protect_content?: boolean;
+            message_id: number;
+        };
+        response: IMessage;
+    };
     copyMessage: {
         request: {
-            chat_id: number | string | undefined, message_thread_id?: number, from_chat_id: string | number,
-            message_id: number, caption?: string, parse_mode?: string, caption_entities?: MessageEventInit[],
-            disable_notification?: boolean, protect_content?: boolean, reply_to_message_id?: number,
-            allow_sending_without_reply?: boolean, reply_markup?: string 
-        },
-        response: IMessage
-    },
+            chat_id: number | string | undefined;
+            message_thread_id?: number;
+            from_chat_id: string | number;
+            message_id: number;
+            caption?: string;
+            parse_mode?: string;
+            caption_entities?: MessageEventInit[];
+            disable_notification?: boolean;
+            protect_content?: boolean;
+            reply_to_message_id?: number;
+            allow_sending_without_reply?: boolean;
+            reply_markup?: string;
+        };
+        response: IMessage;
+    };
     getFile: {
         request: {
-            file_id: string
-        },
-        response: IFile
-    },
+            file_id: string;
+        };
+        response: IFile;
+    };
     getChat: {
         request: {
-            chat_id: number | string
-        },
-        response: IChat
-    }
+            chat_id: number | string;
+        };
+        response: IChat;
+    };
+    getChatAdministrators: {
+        request: {
+            chat_id: number | string;
+        };
+        response: IChatMember[];
+    };
+    getChatMember: {
+        request: {
+            chat_id: number | string;
+            user_id: number;
+        };
+        response: IChatMember;
+    };
 }
--- src/types/response.ts
+++ src/types/response.ts
@@ -3,199 +3,274 @@ export type CallbackData = string;
 export type MessageData = string;
 
 export interface IBasicResponse {
-    ok: boolean,
-    result: any,
-    error_code?: number,
-    description?: string
+    ok: boolean;
+    result: any;
+    error_code?: number;
+    description?: string;
 }
 
 export interface IMessage {
-    text?: string,
-    message_id: number,
-    from?: IUser,
-    sender_chat?: IUser,
-    date: number,
-    chat: IChat,
-    forward_from?: IUser,
-    forward_from_chat?: IChat,
-    forward_from_message_id?: number,
-    forward_signature?: string,
-    forward_sender_name?: string,
-    forward_date?: number,
-    is_automatic_forward?: true,
-    reply_to_message?: IMessage,
-    via_bot?: IUser,
-    edit_date?: number,
-    has_protected_content?: true,
-    media_group_id?: string,
-    author_signature?: string,
-    entities?: IMessageEntity[],
-    reply_markup?: IInlineKeyboardMarkup,
-    web_app_data?: IWebAppData,
-    audio?: IAudio,
-    photo?: IPhotoSize[],
-    voice?: IVoice
+    text?: string;
+    message_id: number;
+    from?: IUser;
+    sender_chat?: IUser;
+    date: number;
+    chat: IChat;
+    forward_from?: IUser;
+    forward_from_chat?: IChat;
+    forward_from_message_id?: number;
+    forward_signature?: string;
+    forward_sender_name?: string;
+    forward_date?: number;
+    is_automatic_forward?: true;
+    reply_to_message?: IMessage;
+    via_bot?: IUser;
+    edit_date?: number;
+    has_protected_content?: true;
+    media_group_id?: string;
+    author_signature?: string;
+    entities?: IMessageEntity[];
+    reply_markup?: IInlineKeyboardMarkup;
+    web_app_data?: IWebAppData;
+    audio?: IAudio;
+    photo?: IPhotoSize[];
+    voice?: IVoice;
 }
 
 export interface IFile {
-    file_id: string,
-    file_unique_id: string,
-    file_size?: number,
-    file_path?: string
+    file_id: string;
+    file_unique_id: string;
+    file_size?: number;
+    file_path?: string;
 }
 
 export interface IVoice {
-    file_id: string,
-    file_unique_id: string,
-    duration: number,
-    mime_type?: string,
-    file_size?: number
+    file_id: string;
+    file_unique_id: string;
+    duration: number;
+    mime_type?: string;
+    file_size?: number;
 }
 
 export interface IAudio {
-    file_id: string,
-    file_unique_id: string,
-    duration: number,
-    performer?: string,
-    title?: string,
-    file_name?: string,
-    mime_type?: string,
-    file_size?: number,
-    thumbnail?: IPhotoSize
+    file_id: string;
+    file_unique_id: string;
+    duration: number;
+    performer?: string;
+    title?: string;
+    file_name?: string;
+    mime_type?: string;
+    file_size?: number;
+    thumbnail?: IPhotoSize;
 }
 
 export interface IPhotoSize {
-    file_id: string,
-    file_unique_id: string,
-    width: number,
-    height: number,
-    file_size?: number
+    file_id: string;
+    file_unique_id: string;
+    width: number;
+    height: number;
+    file_size?: number;
 }
 
 export interface IInlineKeyboardMarkup {
-    inline_keyboard: IInlineKeyboardButton[][]
+    inline_keyboard: IInlineKeyboardButton[][];
 }
 
 export interface IInlineKeyboardButton {
-    text: string,
-    url?: string,
-    callback_data?: CallbackData,
-    web_app?: IWebAppInfo,
-    login_url?: ILoginUrl,
-    switch_inline_query?: string,
-    switch_inline_query_current_chat?: string,
+    text: string;
+    url?: string;
+    callback_data?: CallbackData;
+    web_app?: IWebAppInfo;
+    login_url?: ILoginUrl;
+    switch_inline_query?: string;
+    switch_inline_query_current_chat?: string;
     //callback_game,
-    pay?: boolean
+    pay?: boolean;
 }
 
 export interface IReplyKeyboardMarkup {
-    keyboard: IReplyKeyboardButton[][],
-    resize_keyboard?: boolean,
-    one_time_keyboard?: boolean,
-    input_field_placeholder?: string,
-    selective?: boolean
+    keyboard: IReplyKeyboardButton[][];
+    resize_keyboard?: boolean;
+    one_time_keyboard?: boolean;
+    input_field_placeholder?: string;
+    selective?: boolean;
 }
 
 export interface IReplyKeyboardRemove {
-    remove_keyboard: true,
-    selective?: boolean
+    remove_keyboard: true;
+    selective?: boolean;
 }
 
 export interface IForceReply {
-    force_reply: true,
-    input_field_placeholder?: string,
-    selective?: boolean
+    force_reply: true;
+    input_field_placeholder?: string;
+    selective?: boolean;
 }
 
 export interface IReplyKeyboardButton {
-    text: MessageData,
-    request_contact?: boolean,
-    request_location?: boolean,
-    web_app?: IWebAppInfo
+    text: MessageData;
+    request_contact?: boolean;
+    request_location?: boolean;
+    web_app?: IWebAppInfo;
     // https://core.telegram.org/bots/api#keyboardbutton
 }
 
 export interface ILoginUrl {
-    url: string,
-    forward_text?: string,
-    bot_username?: string,
-    request_write_access?: boolean
+    url: string;
+    forward_text?: string;
+    bot_username?: string;
+    request_write_access?: boolean;
 }
 
 export interface IUpdate {
-    update_id: number,
-    message: IMessage,
-    edited_message: IMessage,
-    channel_post: IMessage,
-    edited_channel_post: IMessage,
-    inline_query: IInlineQuery,
+    update_id: number;
+    message: IMessage;
+    edited_message: IMessage;
+    channel_post: IMessage;
+    edited_channel_post: IMessage;
+    inline_query: IInlineQuery;
     // chosen_inline_result,
-    callback_query: ICallbackQuery
+    callback_query: ICallbackQuery;
     //add more https://core.telegram.org/bots/api#update
 }
 
 export interface IInlineQuery {
-    id: string,
-    from: IUser,
-    query: string,
-    offset: string,
-    chat_type?: string,
+    id: string;
+    from: IUser;
+    query: string;
+    offset: string;
+    chat_type?: string;
 }
 
 export interface IUser {
-    id: number,
-    is_bot: boolean,
-    first_name?: string,
-    last_name?: string,
-    username?: string,
-    language_code?: string,
-    is_premium?: true,
-    added_to_attachment_menu?: true,
-    can_join_groups?: boolean,
-    can_read_all_group_messages?: boolean,
-    supports_inline_queries?: boolean
+    id: number;
+    is_bot: boolean;
+    first_name?: string;
+    last_name?: string;
+    username?: string;
+    language_code?: string;
+    is_premium?: true;
+    added_to_attachment_menu?: true;
+    can_join_groups?: boolean;
+    can_read_all_group_messages?: boolean;
+    supports_inline_queries?: boolean;
 }
 
 export interface IChat {
-    id: number,
-    type: string,
-    title?: string,
-    username?: string,
-    first_name?: string,
-    last_name?: string,
-    bio?: string,
+    id: number;
+    type: string;
+    title?: string;
+    username?: string;
+    first_name?: string;
+    last_name?: string;
+    bio?: string;
 }
 
 export interface ICallbackQuery {
-    id: string,
-    from: IUser,
-    message?: IMessage,
-    inline_message_id?: string,
-    chat_instance: string,
-    data?: CallbackData,
-    game_short_name?: string
+    id: string;
+    from: IUser;
+    message?: IMessage;
+    inline_message_id?: string;
+    chat_instance: string;
+    data?: CallbackData;
+    game_short_name?: string;
 }
 
 export interface IWebAppInfo {
-    url: string
+    url: string;
 }
 
 export interface IWebAppData {
-    data: string,
-    button_text: string
+    data: string;
+    button_text: string;
 }
 
 export interface IMessageId {
-    message_id: number
+    message_id: number;
+}
+
+export type IChatMember = IChatMemberOwner | IChatMemberAdministator | IChatMemberMember | IChatMemberRestricted | IChatMemberLeft | IChatMemberBanned;
+
+export interface IChatMemberOwner {
+    status: "creator";
+    user: IUser;
+    is_anonymous: boolean;
+    custom_title?: string;
+}
+
+export interface IChatMemberAdministator {
+    status: "administrator";
+    user: IUser;
+    can_be_edited: boolean;
+    is_anonymous: boolean;
+    can_manage_chat: boolean;
+    can_delete_messages: boolean;
+    can_manage_voice_chats: boolean;
+    can_restrict_members: boolean;
+    can_promote_members: boolean;
+    can_change_info: boolean;
+    can_invite_users: boolean;
+    can_post_messages: boolean;
+    can_edit_messages: boolean;
+    can_pin_messages: boolean;
+    cam_manage_topics: boolean;
+    custom_title?: string;
+}
+
+export interface IChatMemberMember {
+    status: "member";
+    user: IUser;
+}
+
+export interface IChatMemberRestricted {
+    status: "restricted";
+    user: IUser;
+    is_member: boolean;
+    can_change_info: boolean;
+    can_invite_users: boolean;
+    can_pin_messages: boolean;
+    can_send_messages: boolean;
+    can_send_media_messages: boolean;
+    can_send_polls: boolean;
+    can_send_other_messages: boolean;
+    can_add_web_page_previews: boolean;
+    until_date: number;
+}
+
+export interface IChatMemberLeft {
+    status: "left";
+    user: IUser;
+}
+
+export interface IChatMemberBanned {
+    status: "kicked";
+    user: IUser;
+    until_date: number;
 }
 
 export interface IMessageEntity {
-    type: "mention" | "hashtag" | "cashtag" | "bot_command" | "url" | "email" | "phone_number" | "bold" | "italic" 
-    | "underline" | "strikethrough" | "spoiler" | "code" | "pre" | "text_link" | "text_mention" | "custom_emoji",
-    offset: number,
-    length: number,
-    url?: string,
-    user?: IUser,
-    language?: string,
-    custom_emoji_id?: string
+    type:
+        | "mention"
+        | "hashtag"
+        | "cashtag"
+        | "bot_command"
+        | "url"
+        | "email"
+        | "phone_number"
+        | "bold"
+        | "italic"
+        | "underline"
+        | "strikethrough"
+        | "spoiler"
+        | "code"
+        | "pre"
+        | "text_link"
+        | "text_mention"
+        | "custom_emoji";
+    offset: number;
+    length: number;
+    url?: string;
+    user?: IUser;
+    language?: string;
+    custom_emoji_id?: string;
 }
